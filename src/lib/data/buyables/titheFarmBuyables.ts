3d794b864db62a003a777be15e2c6caa8671f5e8
gc <30398469+gc@users.noreply.github.com>
2023-03-18 14:40:06 +1100

    changes

--- src/lib/data/buyables/titheFarmBuyables.ts
+++ src/lib/data/buyables/titheFarmBuyables.ts
@@ -1,9 +1,10 @@
-import { ItemBank } from '../../types';
+import { Bank } from 'oldschooljs';
+
 import itemID from '../../util/itemID';
 
 interface TitheFarmBuyable {
 	name: string;
-	outputItems: ItemBank;
+	outputItems: Bank;
 	titheFarmPoints: number;
 	aliases?: string[];
 }
@@ -12,95 +13,71 @@ const TitheFarmBuyables: TitheFarmBuyable[] = [
 	{
 		name: "Farmer's strawhat",
 		aliases: ['farmers strawhat', 'farmers hat'],
-		outputItems: {
-			[itemID("Farmer's strawhat")]: 1
-		},
+		outputItems: new Bank().add("Farmer's strawhat"),
 		titheFarmPoints: 75
 	},
 	{
 		name: "Farmer's jacket",
 		aliases: ['farmers jacket'],
-		outputItems: {
-			[itemID("Farmer's jacket")]: 1
-		},
+		outputItems: new Bank().add("Farmer's jacket"),
 		titheFarmPoints: 150
 	},
 	{
 		name: "Farmer's shirt",
 		aliases: ['farmers shirt'],
-		outputItems: {
-			[itemID("Farmer's shirt")]: 1
-		},
+		outputItems: new Bank().add("Farmer's shirt"),
 		titheFarmPoints: 150
 	},
 	{
 		name: "Farmer's boro trousers",
 		aliases: ['farmers trousers', 'farmers bottoms', 'farmers legs'],
-		outputItems: {
-			[itemID("Farmer's boro trousers")]: 1
-		},
+		outputItems: new Bank().add("Farmer's boro trousers"),
 		titheFarmPoints: 125
 	},
 	{
 		name: "Farmer's boots",
 		aliases: ['farmers boots'],
-		outputItems: {
-			[itemID("Farmer's boots")]: 1
-		},
+		outputItems: new Bank().add("Farmer's boots"),
 		titheFarmPoints: 50
 	},
 	{
 		name: 'Seed box',
-		outputItems: {
-			[itemID('Seed box')]: 1
-		},
+		outputItems: new Bank().add('Seed box', 1),
 		titheFarmPoints: 250
 	},
 	{
 		name: "Gricoller's can",
 		aliases: ['can', 'gricollers can'],
-		outputItems: {
-			[itemID("Gricoller's can")]: 1
-		},
+		outputItems: new Bank().add("Gricoller's can"),
 		titheFarmPoints: 200
 	},
 	{
 		name: 'Herb sack',
-		outputItems: {
-			[itemID('Herb sack')]: 1
-		},
+		outputItems: new Bank().add('Herb sack', 1),
 		titheFarmPoints: 250
 	},
 	{
 		name: 'Grape seed',
 		aliases: ['grape seeds'],
-		outputItems: {
-			[itemID('Grape seed')]: 1
-		},
+		outputItems: new Bank().add('Grape seed', 1),
 		titheFarmPoints: 2
 	},
 	{
 		name: '20x Grape seed',
 		aliases: ['20x grape seeds'],
-		outputItems: {
-			[itemID('Grape seed')]: 20
-		},
+		outputItems: new Bank().add('Grape seed', 20),
 		titheFarmPoints: 40
 	},
 	{
 		name: 'Supercompost',
 		aliases: ['super compost'],
-		outputItems: {
-			[itemID('Supercompost')]: 1
-		},
+		outputItems: new Bank().add('Supercompost', 1),
 		titheFarmPoints: 5
 	},
 	{
 		name: "Bologa's blessing",
 		aliases: ['bologa'],
-		outputItems: {
-			[itemID("Bologa's blessing")]: 10
-		},
+		outputItems: new Bank().add("Bologa's blessing", 10),
 		titheFarmPoints: 1
 	}
 ];
